1a2b5db6fa2e1a77ea0c419f1779e68089bf3bbc
Ashutosh Pandey <uncreatedfool@gmail.com>
2021-06-25 19:49:34 +0530

    Second Commit

--- server.js
+++ server.js
@@ -7,25 +7,16 @@ import employeeRoutes from "./routes/employeeRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 dotenv.config();
 app.use(express.json());
 connectDB();
 app.use("/api/employees", employeeRoutes);
 app.use(errorHandler);
 // app.use(notFound);
-
-// const __dirname = path.resolve();
-// console.log(__dirname);
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/quizmaker/build")));
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "quizmaker", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API IS RUNNING");
-//   });
-// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("SERVER STARTED"));
