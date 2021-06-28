84e079dbb5b7fecde26fd8f2d6c030b923162a0a
Ashutosh Pandey <ashutosh@Ashutoshs-MacBook-Pro.local>
2021-06-28 21:36:07 +0530

    Added put and delete employee

--- controllers/emloyeeController.js
+++ controllers/emloyeeController.js
@@ -49,3 +49,55 @@ export const getAllEmployees = asyncHandler(async (req, res) => {
     throw new Error("Employees not found");
   }
 });
+
+
+// @desc Delete employee
+// @route DELETE /api/employees/:id
+// @access public
+
+export const deleteEmployee =asyncHandler(async (req,res)=>{
+  
+    // console.log(req.params.id);
+    const employee = await Employee.findById(req.params.id)
+    // console.log(employee);
+    if (employee) {
+      await employee.remove()
+      res.json({ message: 'Employee removed' })
+    } else {
+      res.status(404)
+      throw new Error('Employee not found')
+    }
+})
+
+
+
+// @desc Update employee
+// @route Update /api/employees/:id
+// @access public
+export const updateEmployee = asyncHandler(async (req, res) => {
+  const employee = await Employee.findById(req.params.id)
+  const {
+    firstName,
+    lastName,
+    spouse,
+    martial_status,
+    comments,
+    gender,
+  } = req.body;
+  employee.firstName=firstName;
+  employee.lastName=lastName;
+  employee.martial_status=martial_status;
+  employee.comments=comments;
+  employee.gender=gender;
+  employee.spouse=spouse;
+
+  if (employee) {
+    const updatedEmployee = await employee.save()
+    res.json({
+      employee
+    })
+  } else {
+    res.status(404)
+    throw new Error('Employee not found')
+  }
+})
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 import {
   postEmployee,
   getAllEmployees,
+  deleteEmployee,
+  updateEmployee
 } from "../controllers/emloyeeController.js";
 
 router.route("/").post(postEmployee).get(getAllEmployees);
+router.route("/:id").delete(deleteEmployee).put(updateEmployee);
 export default router;
